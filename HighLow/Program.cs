b2bfd8999f6c32919db6e3d6b7fa3aca28af6aaa
Kristen Shea <kstar61@live.com>
2019-03-23 17:56:19 -0500

    /

--- HighLow/Program.cs
+++ HighLow/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HighLow
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string numbers = "1 -22 3 -4 55";
+
+            List<string> numList = numbers.Split(' ').ToList();
+
+            List<int> numParseList = new List<int>();
+
+            foreach (var item in numList)
+            {
+                int numParse = int.Parse(item);
+                numParseList.Add(numParse);
+            }
+
+            int highNum = numParseList.Max();
+
+            var lowNum = numParseList.Min();
+
+            string answer = highNum + " " + lowNum;
+
+            Console.WriteLine($"{answer}");
+
+            Console.ReadLine();
         }
     }
 }
